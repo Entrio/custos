a1e9ef03164ef2b0895e61b4124e5f5ca67ccf6f
Alexander Titarenko <westal@gmail.com>
2021-02-26 14:46:02 +0600

    Basic RBAC is done
    * Need to implement HRBAC

--- internal/handler.go
+++ internal/handler.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"fmt"
 	"github.com/labstack/echo/v4"
+	"strings"
 )
 
 //region Common handlers
@@ -35,15 +36,55 @@ func processOathkeeperRequest(c echo.Context) error {
 		return jsonError(or, 403, "User has been deleted", nil)
 	}
 
+	service := new(Service)
+	if err := dbInstance.Model(&Service{}).First(service, "name", or.Payload.Service); err.Error != nil {
+		return jsonError(or, 403, "Failed to find service", err.Error)
+	}
+
+	groups := []Group{}
+	gids := []string{}
+
+	if err := dbInstance.Table("user_group").Select("group_id").Where("user_id", user.ID).Find(&gids); err.Error != nil {
+		return jsonError(or, 403, "Failed to find user groups", err.Error)
+	}
+
+	if len(gids) == 0 {
+		return jsonError(or, 403, "Access denied", nil)
+	}
+
+	if err := dbInstance.Model(&Group{}).Where("id in ?", gids).Find(&groups); err.Error != nil {
+		return jsonError(or, 403, "Failed to find groups", err.Error)
+	}
+
+	verb := new(Verb)
+	if err := dbInstance.Model(&Verb{}).Where("name = ?", strings.ToUpper(or.Payload.Verb)).First(&verb); err.Error != nil {
+		return jsonError(or, 403, "Failed to find verb", err.Error)
+	}
+
+	fmt.Println(fmt.Sprintf("User %s trying to access service %s via the %s method", user.FirstName, service.Name, verb.Name))
+
+	accessGranted := false
+
+	match := int64(0)
+	if err := dbInstance.Table("service_group_verb").Where("service_id = ? AND verb_id = ? AND group_id = ?", service.ID, verb.ID, groups[0].ID).Count(&match); err.Error != nil {
+		return jsonError(or, 403, "Access denied", nil)
+	}
+
+	if match == 0 {
+		return jsonError(or, 403, "Access denied", nil)
+	}
+
+	accessGranted = true
+
 	/**
 	This is the logic that we need to process:
 	1) The request comes in. We collect all of the following:
-	  1.1) Service - make sure it exists
-	  1.2) Model - make sure it exists in the database
-	  1.3) Verb - make sure its within valid ranges
-	2) We fetch user's group
-	  2.1) If no groups are found, flow is terminated and access is denied
-	  2.2) If group has a parent group, repeat steps 2.2 onwards until flow is terminated for each parent group
+	  1.1) Service - make sure it exists ✓
+	  1.2) Model - make sure it exists in the database (NOT YET IMPLEMENTED)
+	  1.3) Verb - make sure its within valid ranges ✓
+	2) We fetch user's group(s) ✓
+	  2.1) If no groups are found, flow is terminated and access is denied ✓
+	  2.2) If group has a parent group, repeat steps 2.2 onwards until flow is terminated for each parent group (NOT YET IMPLEMENTED)
 	3) We fetch the group's service action for that verb
 	  3.1) If the action is deny, the flow is terminated and access is denied
 	  3.2) If there are no matches found or the action is allowed, we evaluate model access
@@ -53,11 +94,14 @@ func processOathkeeperRequest(c echo.Context) error {
 
 	fmt.Println("\n##### HRBAC END #####\b")
 
-	return or.JSON(200, struct {
-		Message string `json:"message"`
-	}{
-		Message: "Hello, world",
-	})
+	if accessGranted {
+		return or.JSON(200, struct {
+			Message string `json:"message"`
+		}{
+			Message: "Hello, world",
+		})
+	}
+	return jsonError(or, 403, "Access denied", nil)
 }
 
 //endregion
