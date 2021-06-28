c21cdf1593f567da85daa1fde0a43baf42074948
Titarenko Alexander <ro12@vrcode.kz>
2021-06-28 16:43:09 +0600

    Added global env settings that can change CORS domains
     + Added ALLOWED_CORS for public routes
     + Added ADMIN_ALLOWED_CORS for admin routes

--- internal/router.go
+++ internal/router.go
@@ -2,7 +2,11 @@ package internal
 
 import (
 	"fmt"
+	"github.com/Entrio/subenv"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"log"
+	"strings"
 )
 
 func RegisterRoutes(e *echo.Echo) *echo.Echo {
@@ -10,6 +14,20 @@ func RegisterRoutes(e *echo.Echo) *echo.Echo {
 		e = echo.New()
 		fmt.Println("New echo from RegisterRoutes")
 	}
+
+	origins := []string{"http://127.0.0.1:8080", "http://192.168.1.33:8080"}
+
+	if subenv.Env("ALLOWED_CORS", "") != "" {
+		origins = strings.Split(subenv.Env("ALLOWED_CORS", ""), "|")
+	}
+
+	log.Println("[PUB] Allowed CORS domains:", origins)
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowCredentials: true,
+		AllowOrigins:     origins,
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAccessControlAllowCredentials},
+	}))
 
 	// Register routes
 	e.POST("*", processOathkeeperRequest, validateOathkeeperRequest)
@@ -22,6 +40,20 @@ func RegisterAdminRoutes(e *echo.Echo) *echo.Echo {
 		e = echo.New()
 		fmt.Println("New echo from RegisterAdminRoutes")
 	}
+
+	origins := []string{"http://127.0.0.1:8080", "http://192.168.1.33:8080"}
+
+	if subenv.Env("ADMIN_ALLOWED_CORS", "") != "" {
+		origins = strings.Split(subenv.Env("ADMIN_ALLOWED_CORS", ""), "|")
+	}
+
+	log.Println("[ADM] Allowed CORS domains:", origins)
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowCredentials: true,
+		AllowOrigins:     origins,
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAccessControlAllowCredentials},
+	}))
 
 	registerAdminRoutes(e)
 
